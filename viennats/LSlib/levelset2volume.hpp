viennats/viennats-dev

commit e1ae3fb552d57150b4a6187ea55b341132209cd0

Fixed #16 and adapted volume output to work with remove_bottom functionality. (#18)

CMakeLists.txt
@@ -96,8 +96,11 @@ IF(VTK_FOUND)
 message(STATUS "VTK found (include: ${VTK_INCLUDE_DIRS}, lib: ${VTK_LIBRARY_DIRS})")
 include(${VTK_USE_FILE})
 #only link necessary VTK libraries in order for static linking to work (-lGL fails)
-set(VTK_LIBRARIES vtksys;vtkFiltersCore;vtkFiltersGeneral;vtkFiltersGeometry;vtkIOXML)
-message(STATUS "VTK Libraries: ${VTK_LIBRARIES}")
+IF(VIENNATS_STATIC_BUILD)
+  set(VTK_LIBRARIES vtksys;vtkFiltersCore;vtkFiltersGeneral;vtkFiltersGeometry;vtkIOXML)
+  message(STATUS "Static build: Only including following VTK Libraries: ${VTK_LIBRARIES}")
+ENDIF(VIENNATS_STATIC_BUILD)
+
 ELSE(VTK_FOUND)
 message(STATUS "VTK not found.")
 ENDIF(VTK_FOUND)

viennats/LSlib/levelset2volume.hpp
@@ -23,8 +23,13 @@ namespace lvlset{
   // This function takes a levelset and converts it to a vtkRectilinearGrid
   // The full domain contains values, which are capped at numLayers * gridDelta
   // gridExtraPoints layers of grid points are added to the domain according to boundary conditions
-  template<int gridExtraPoints=0, class LevelSetType>
-  vtkSmartPointer<vtkRectilinearGrid> LS2RectiLinearGrid(const LevelSetType& LevelSet, const double LSOffset=0., int infiniteMinimum=std::numeric_limits<int>::max()){
+  template<bool removeBottom=false, int gridExtraPoints=0, class LevelSetType>
+  vtkSmartPointer<vtkRectilinearGrid> LS2RectiLinearGrid(const LevelSetType& LevelSet,
+      const double LSOffset=0.,
+      int infiniteMinimum=std::numeric_limits<int>::max(),
+      int infiniteMaximum=-std::numeric_limits<int>::max()){
+
+
     typedef typename LevelSetType::grid_type2 GridType;
     static const int D=GridType::dimensions;
 
@@ -39,6 +44,7 @@ namespace lvlset{
 
     vtkSmartPointer<vtkFloatArray> coords[3]; // needs to be 3 because vtk only knows 3D
     int gridMin=0, gridMax=0;
+    int openJumpDirection=-1;  // direction above the open boundary direction
 
 
     // fill grid with offset depending on orientation
@@ -48,11 +54,13 @@ namespace lvlset{
       if(LevelSet.grid().boundary_conditions(i)==lvlset::INFINITE_BOUNDARY){
         // add one to gridMin and gridMax for numerical stability
         gridMin = std::min(LevelSet.get_min_runbreak(i), infiniteMinimum)-1;  // choose the smaller number so that for first levelset the overall minimum can be chosen
-        gridMax = LevelSet.get_max_runbreak(i)+1;
+        gridMax = std::max(LevelSet.get_max_runbreak(i), infiniteMaximum)+1;
 
         for(int x=gridMin; x <= gridMax; ++x){
           coords[i]->InsertNextValue(x * gridDelta);
         }
+
+        openJumpDirection=i+1;
 
       }else{
         gridMin = LevelSet.grid().min_grid_index(i)-gridExtraPoints;
@@ -83,12 +91,19 @@ namespace lvlset{
     rgrid->SetZCoordinates(coords[2]);
 
 
+
+
     // now iterate over grid and fill with LS values
     // initialise iterator over levelset and pointId to start at gridMin
     vtkIdType pointId=0;
     bool fixBorderPoints = (gridExtraPoints!=0);
 
     typename LevelSetType::const_iterator_runs it_l(LevelSet);
+    // need to save the current position one dimension above open boundary direction,
+    // so we can register a jump in the open boundary direction when it occurs, so we
+    // can fix the LS value as follows: if remove_bottom==true, we need to flip the sign
+    // otherwise the sign stays the same
+    int currentOpenIndex = it_l.end_indices()[(openJumpDirection<D)?openJumpDirection:0];
 
     // Make array to store signed distance function
     vtkSmartPointer<vtkFloatArray> signedDistances =
@@ -117,6 +132,13 @@ namespace lvlset{
           LSValue = numLayers;
         }else if(LSValue == LevelSetType::NEG_VALUE){
           LSValue = - numLayers;
+        }
+
+        if(removeBottom){
+          if(currentOpenIndex!=indices[openJumpDirection]){
+            LSValue = -LSValue;
+            currentOpenIndex=indices[openJumpDirection];
+          }
         }
 
         signedDistances->InsertNextValue(LSValue * gridDelta);
@@ -187,7 +209,7 @@ namespace lvlset{
   // since the top levelset wraps all lower ones, all levelsets below it are used to cut this mesh
   // explicitly. When all the cuts are made, material numbers are assigned and the tetra meshes written
   // to one single file
-  template<class LevelSetsType>
+  template<bool removeBottom, class LevelSetsType>
   void extract_volume(const LevelSetsType& LevelSets, vtkSmartPointer<vtkUnstructuredGrid>& volumeMesh){
 
     typedef typename LevelSetsType::value_type::grid_type2 GridType;
@@ -195,16 +217,19 @@ namespace lvlset{
 
     // number of levels required to output
     constexpr int numLayers = 2;
+    assert(LevelSets.rbegin()->number_of_layers()>=numLayers);  // check if enough information
 
     // store volume for each material
     std::vector< vtkSmartPointer<vtkUnstructuredGrid> > materialMeshes;
 
 
     int totalMinimum = std::numeric_limits<int>::max();
+    int totalMaximum = -std::numeric_limits<int>::max();
     for(auto it=LevelSets.begin(); it!=LevelSets.end(); ++it){
       for(unsigned i=0; i<D; ++i){
         if(it->grid().boundary_conditions(i)==lvlset::INFINITE_BOUNDARY){
           totalMinimum = std::min(totalMinimum, it->get_min_runbreak(i));
+          totalMaximum = std::max(totalMaximum, it->get_max_runbreak(i));
         }
       }
     }
@@ -214,7 +239,7 @@ namespace lvlset{
     // Use vtkClipDataSet to slice the grid
     vtkSmartPointer<vtkTableBasedClipDataSet> clipper =
       vtkSmartPointer<vtkTableBasedClipDataSet>::New();
-    clipper->SetInputData(LS2RectiLinearGrid(*(LevelSets.rbegin()), 0, totalMinimum));  // last LS
+    clipper->SetInputData(LS2RectiLinearGrid<removeBottom>(*(LevelSets.rbegin()), 0, totalMinimum, totalMaximum));  // last LS
     clipper->InsideOutOn();
     clipper->Update();
 
@@ -229,7 +254,7 @@ namespace lvlset{
 
       // create grid of next LS with slight offset and project into current mesh
       vtkSmartPointer<vtkRectilinearGrid> rgrid = vtkSmartPointer<vtkRectilinearGrid>::New();
-      rgrid = LS2RectiLinearGrid<1>(*it, -1e-5);  // number of extra grid points outside and LSOffset
+      rgrid = LS2RectiLinearGrid<removeBottom, 1>(*it, -1e-5);  // number of extra grid points outside and LSOffset
 
       // now transfer implicit values to mesh points
       vtkSmartPointer<vtkProbeFilter> probeFilter = vtkSmartPointer<vtkProbeFilter>::New();

viennats/LSlib/output.hpp
@@ -223,16 +223,25 @@ namespace lvlset {
         write_explicit_surface_opendx(l, filename, DefaultDataType(), eps);
     }
 
-    template <class LevelSetsType>
-    void write_explicit_volume_vtk(const LevelSetsType& LevelSets, const std::string& fileName, double eps=0.) {
-      write_explicit_volume_vtk(LevelSets, fileName, DefaultDataType(), eps);
+    template <class LevelSetsType, class ParameterType>
+    void write_explicit_volume_vtk(const LevelSetsType& LevelSets, const std::string& fileName, ParameterType& p, double eps=0.) {
+      write_explicit_volume_vtk(LevelSets, fileName, p, DefaultDataType(), eps);
     }
 
-    template <class LevelSetsType, class DataType>
-    void write_explicit_volume_vtk(const LevelSetsType& LevelSets, const std::string& fileName, const DataType& Data, double eps=0.) {
+    template <class LevelSetsType, class ParameterType, class DataType>
+    void write_explicit_volume_vtk(const LevelSetsType& LevelSets, const std::string& fileName, ParameterType& p, const DataType& Data, double eps=0.) {
+      static const int D=LevelSetsType::value_type::grid_type2::dimensions;
 
       vtkSmartPointer<vtkUnstructuredGrid> volumeMesh = vtkSmartPointer<vtkUnstructuredGrid>::New();
-      extract_volume(LevelSets, volumeMesh);
+
+      // depending on the open boundary, volume output needs to be treated differently if the bottom is removed
+      if(p.remove_bottom &&
+       (p.open_boundary<(D-1))){
+        extract_volume<true>(LevelSets, volumeMesh);
+      }else{
+        extract_volume<false>(LevelSets, volumeMesh);
+      }
+
 
       vtkSmartPointer<vtkXMLUnstructuredGridWriter> owriter =
         vtkSmartPointer<vtkXMLUnstructuredGridWriter>::New();

viennats/Process.h
@@ -1704,7 +1704,7 @@ namespace proc {
             std::ostringstream oss;
             oss << Parameter.output_path<< "Volume_" << output_info.output_counter << ".vtu";
 
-            lvlset::write_explicit_volume_vtk(LevelSets, oss.str());
+            lvlset::write_explicit_volume_vtk(LevelSets, oss.str(), Parameter);
           }
 
           output_info.output_counter++;

viennats/Geometry.h
@@ -933,7 +933,7 @@ namespace geometry {
 
           for (int k=0;k<D;k++) tmp[k]=Geometry.Elements[i][(j+k)%(D+1)];
 
-          /*std::bitset<2*D> flags;
+          std::bitset<2*D> flags;
           flags.set();
 
           //if triangle at border skip
@@ -951,9 +951,10 @@ namespace geometry {
 
           flags &=remove_flags;
 
-          //if (is_open_boundary_negative) flags.reset(open_boundary_direction); else flags.reset(open_boundary_direction+D);
+          //if (is_open_boundary_negative) flags.reset(open_boundary_direction);
+          //else flags.reset(open_boundary_direction+D);
 
-          if (flags.any()) continue;*/
+          if (flags.any()) continue;
 
           tmp.sort();
 

commit 208e331aeeeb1b929afe4dcb12cb6d6457c931bc

Fixed mask input and added remove_levelset to BooleanOperation. (#20)
* Fixed mask import error for smaller-than-domain masks re-introduced in #18 
* Added remove_levelset in BooleanOpeartion
* Improved volume hull and tetra output efficiency

CMakeLists.txt
@@ -8,7 +8,7 @@
 PROJECT(ViennaLS)
 
 set(VERSION_MAJOR 2)
-set(VERSION_MINOR 2)
+set(VERSION_MINOR 3)
 set(VERSION_PATCH 0)
 set(VERSION ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH})
 

viennats/LSlib/levelset2volume.hpp
@@ -11,6 +11,7 @@
 #include <vtkPointData.h>
 #include <vtkTableBasedClipDataSet.h>
 #include <vtkDataSetTriangleFilter.h>
+#include <vtkTriangleFilter.h>
 #include <vtkAppendFilter.h>
 #include <vtkAppendPolyData.h>
 #include <vtkProbeFilter.h>
@@ -355,20 +356,29 @@ namespace lvlset{
       appendFilter->AddInputData(materialMeshes[materialMeshes.size()-1-i]);
     }
 
-    appendFilter->Update();
+    // do not need tetrahedral volume mesh if we do not print volume
+    if(volumeMesh.GetPointer() != 0){
+      appendFilter->Update();
 
-    // change all 3D cells into tetras and all 2D cells to triangles
-    vtkSmartPointer<vtkDataSetTriangleFilter> triangleFilter =
-      vtkSmartPointer<vtkDataSetTriangleFilter>::New();
-    triangleFilter->SetInputConnection(appendFilter->GetOutputPort());
-    triangleFilter->Update();
+      // change all 3D cells into tetras and all 2D cells to triangles
+      vtkSmartPointer<vtkDataSetTriangleFilter> triangleFilter =
+        vtkSmartPointer<vtkDataSetTriangleFilter>::New();
+      triangleFilter->SetInputConnection(appendFilter->GetOutputPort());
+      triangleFilter->Update();
 
-    volumeMesh = triangleFilter->GetOutput();
+      volumeMesh = triangleFilter->GetOutput();
+    }
+
 
     // Now make hull mesh if necessary
     if(hullMesh.GetPointer() != 0){
       hullAppendFilter->Update();
-      hullMesh = hullAppendFilter->GetOutput();
+
+      vtkSmartPointer<vtkTriangleFilter> hullTriangleFilter = vtkSmartPointer<vtkTriangleFilter>::New();
+      hullTriangleFilter->SetInputConnection(hullAppendFilter->GetOutputPort());
+      hullTriangleFilter->Update();
+
+      hullMesh = hullTriangleFilter->GetOutput();
     }
   }
 
@@ -377,7 +387,7 @@ namespace lvlset{
   template<bool removeBottom, class LevelSetsType>
   void extract_volume(const LevelSetsType& LevelSets,
       vtkSmartPointer<vtkUnstructuredGrid>& volumeMesh){
-    vtkSmartPointer<vtkPolyData> dummyHull = vtkSmartPointer<vtkPolyData>::New();
+    vtkSmartPointer<vtkPolyData> dummyHull;
     extract_volume<removeBottom>(LevelSets, volumeMesh, dummyHull);
   }
 

viennats/LSlib/output.hpp
@@ -235,17 +235,22 @@ namespace lvlset {
     void write_explicit_volume_vtk(const LevelSetsType& LevelSets, const CounterType counter, ParameterType& p, const DataType& Data, double eps=0.) {
       static const int D=LevelSetsType::value_type::grid_type2::dimensions;
 
-      vtkSmartPointer<vtkUnstructuredGrid> volumeMesh = vtkSmartPointer<vtkUnstructuredGrid>::New();
-      vtkSmartPointer<vtkPolyData> hullMesh = vtkSmartPointer<vtkPolyData>::New();
+      vtkSmartPointer<vtkUnstructuredGrid> volumeMesh;
+      vtkSmartPointer<vtkPolyData> hullMesh;
+
+      // check if any output should be done. if none, we do not need to extract the volume
+      if(!(p.print_volume_tetra || p.print_volume_hull)) return;
+
+      // if volumeMesh or hullMesh are not initialised, they will not be created in extract_volume
+      if(p.print_volume_tetra) volumeMesh = vtkSmartPointer<vtkUnstructuredGrid>::New();
+      if(p.print_volume_hull) hullMesh = vtkSmartPointer<vtkPolyData>::New();
 
       // depending on the open boundary, volume output needs to be treated differently if the bottom is removed
       if(p.remove_bottom &&
        (p.open_boundary<(D-1))){
-        if(p.print_volume_hull) extract_volume<true>(LevelSets, volumeMesh, hullMesh);
-        else extract_volume<true>(LevelSets, volumeMesh);
+        extract_volume<true>(LevelSets, volumeMesh, hullMesh);
       }else{
-        if(p.print_volume_hull) extract_volume<false>(LevelSets, volumeMesh, hullMesh);
-        else extract_volume<false>(LevelSets, volumeMesh);
+        extract_volume<false>(LevelSets, volumeMesh, hullMesh);
       }
 
       if(p.print_volume_tetra){

viennats/Model/ModelBooleanOps.h
@@ -32,6 +32,8 @@ namespace model {
 
         bool wrap;
 
+        bool removeLS;
+
         int lvlset;
 
     public:
@@ -63,6 +65,10 @@ namespace model {
             return wrap;
         }
 
+        const bool remove_levelset() const{
+          return removeLS;
+        }
+
         BooleanOps(const std::string & Parameters):lvl(0),inv(false),wrap(false),lvlset(-1)  {
             using namespace boost::spirit::classic;
             using namespace parser_actors;
@@ -70,6 +76,8 @@ namespace model {
             remove=true;
             surf=false;
             lvlset=-1;
+            removeLS=false;
+            lvl=0;
 
             bool b = parse(
                     Parameters.begin(),
@@ -81,7 +89,8 @@ namespace model {
                             (str_p("surface_geometry")  >> '='  >> ((str_p("true") | str_p("false"))[assign_bool(surf)]) >> ';') |
                             (str_p("remove_bottom")  >> '='  >> ((str_p("true") | str_p("false"))[assign_bool(remove)]) >> ';') |
                             (str_p("wrap_lower_surface") >> '=' >> ((str_p("true") | str_p("false"))[assign_bool(wrap)]) >> ';') |
-                            (str_p("levelset") >> '=' >> int_p[assign_a(lvlset)] >> ';')
+                            (str_p("levelset") >> '=' >> int_p[assign_a(lvlset)] >> ';') |
+                            (str_p("remove_levelset") >> '=' >> ((str_p("true") | str_p("false"))[assign_bool(removeLS)]) >> ';')
                     ),
                     space_p | comment_p("//") | comment_p("/*", "*/")).full;
 

viennats/Model/ModelPlanarization.h
@@ -36,6 +36,8 @@ namespace model {
             using namespace boost::spirit::classic;
             using namespace parser_actors;
 
+            Fill=false;
+
             bool b = parse(
                     Parameters.begin(),
                     Parameters.end(),

viennats/Process.h
@@ -522,6 +522,14 @@ namespace proc {
             Parameter.change_input_parity, Parameter.material_mapping, Parameter.input_shift, Parameter.ignore_materials);
         }
 
+        // manually set min and max to match original simulation
+        for(unsigned i=0; i<D; ++i){
+          if(LevelSets.back().grid().boundary_conditions(i) != lvlset::INFINITE_BOUNDARY){
+            mask_geometry.Min[i] = LevelSets.back().grid().min_grid_index(i)*Parameter.grid_delta;
+            mask_geometry.Max[i] = LevelSets.back().grid().max_grid_index(i)*Parameter.grid_delta;
+          }
+        }
+
         //      mask_geometry.Read(Model.file_name(), Parameter.input_scale, Parameter.input_transformation, Parameter.input_transformation_signs, Parameter.change_input_parity, Parameter.material_mapping, Parameter.input_shift, Parameter.ignore_materials);
 
         typedef std::list<geometry::surface<D> > SurfacesType;
@@ -592,8 +600,6 @@ namespace proc {
                 const ProcessParameterType& ProcessParameter,
                 OutputInfoType & output_info
         ) {
-
-      if (Model.level()==0) return;
 
         typedef typename LevelSetsType::value_type LevelSetType;
         const int D=LevelSetType::dimensions;
@@ -667,34 +673,42 @@ namespace proc {
             while (ls_it!=LevelSets.end()) {
                 ls_it->min(*boolop_ls);
                 ls_it->prune();
-        ls_it->segment();
+                ls_it->segment();
                 ++ls_it;
             }
 
       if (Model.invert() && Model.levelset()>=0) boolop_ls->invert();    //Invert again so that the original levelset is not changed
-        } else {                        //Model.level()<0
+    } else if(Model.level()<0){
 
-            if (Model.invert()) boolop_ls->invert();
+      if (Model.invert()) boolop_ls->invert();
 
-            int j=0;
-            typename LevelSetsType::iterator ls_it_old  =   LevelSets.begin();
-            typename LevelSetsType::iterator ls_it      =   LevelSets.begin();
+      int j=0;
+      typename LevelSetsType::iterator ls_it_old  =   LevelSets.begin();
+      typename LevelSetsType::iterator ls_it      =   LevelSets.begin();
 
-            for (;j<static_cast<int>(LevelSets.size())+Model.level();++j) {
-                ls_it_old=ls_it;
-                ++ls_it;
-            }
+      for (;j<static_cast<int>(LevelSets.size())+Model.level();++j) {
+        ls_it_old=ls_it;
+        ++ls_it;
+      }
       if(!Model.wrap_surface()) j=0;
 
-            while (ls_it!=LevelSets.end()) {
-                ls_it->max(*boolop_ls);
-                if (j>0) ls_it->min(*ls_it_old);
-                ls_it->prune();
+      while (ls_it!=LevelSets.end()) {
+        ls_it->max(*boolop_ls);
+        if (j>0) ls_it->min(*ls_it_old);
+        ls_it->prune();
         ls_it->segment();
-                ++ls_it;
-            }
+        ++ls_it;
+      }
       if (Model.invert() && Model.levelset()>=0) boolop_ls->invert();    //Invert again so that the original levelset is not changed
-        }
+    }
+
+    // remove levelset used for booling if specified
+    if(Model.levelset()>=0 && Model.remove_levelset()){
+      auto it=LevelSets.begin();
+      for(int i=0; i<Model.levelset(); ++i) ++it;
+      LevelSets.erase(it);
+    }
+
     //Write one output if there is any output time or there is final output
     if(!(!ProcessParameter.output_times.empty() || ProcessParameter.final_output)) return;
 